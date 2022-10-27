acd735963869c1e72705960f72a344ae8d2d15d0
Matheus <matheuseduardo.jp@gmail.com>
2022-10-27 02:35:00 -0300

    FEAT: Requisito 07

--- desafio7.sql
+++ desafio7.sql
@@ -1,26 +1,12 @@
-DROP DATABASE IF EXISTS SpotifyClone;
-
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+SELECT 
+    A.artist_name AS artista,
+    AL.album_name AS album,
+    COUNT(F.user_id) AS seguidores
+FROM
+    SpotifyClone.artist AS A
+        INNER JOIN
+    SpotifyClone.album AS AL ON A.artist_id = AL.artist_id
+        INNER JOIN
+    SpotifyClone.following AS F ON A.artist_id = F.artist_id
+GROUP BY artista, album
+ORDER BY seguidores DESC , artista ASC, album ASC;
