b509d2b515093ea460d648abf9844116c68a1005
Matheus <matheuseduardo.jp@gmail.com>
2022-10-27 02:02:21 -0300

    FEAT: Requisito 06

--- desafio6.sql
+++ desafio6.sql
@@ -1,26 +1,9 @@
-DROP DATABASE IF EXISTS SpotifyClone;
-
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+SELECT 
+    MIN(P.plan_value) AS faturamento_minimo,
+    MAX(P.plan_value) AS faturamento_maximo,
+    ROUND(AVG(P.plan_value), 2) AS faturamento_medio,
+    SUM(P.plan_value) AS faturamento_total
+FROM
+    SpotifyClone.plan AS P
+        INNER JOIN
+    SpotifyClone.user AS U ON P.plan_id = U.plan_id;
