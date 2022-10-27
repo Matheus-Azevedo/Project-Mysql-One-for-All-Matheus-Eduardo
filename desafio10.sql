8aa89d037097017dbe69936469b1d665bdc4ac5a
Matheus <matheuseduardo.jp@gmail.com>
2022-10-27 03:01:43 -0300

    FEAT: Requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,26 +1,12 @@
-DROP DATABASE IF EXISTS SpotifyClone;
-
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+SELECT 
+   M.music_name AS nome,
+   COUNT(H.user_id)  AS reproducoes
+FROM
+    SpotifyClone.music AS M
+        INNER JOIN
+    SpotifyClone.historic AS H ON M.music_id = H.music_id
+		INNER JOIN
+    SpotifyClone.user AS U ON U.user_id = H.user_id
+WHERE U.plan_id = 1 OR U.plan_id = 4
+GROUP BY nome
+ORDER BY nome;
