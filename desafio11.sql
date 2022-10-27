0f54d9f3e889073263276f781b30a2c8fd29d09f
Matheus <matheuseduardo.jp@gmail.com>
2022-10-27 03:30:23 -0300

    FEAT: Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,26 +1,34 @@
-DROP DATABASE IF EXISTS SpotifyClone;
-
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+SELECT 
+    music_name AS nome_musica,
+    CASE
+        WHEN music_name LIKE '%Bard%' THEN REPLACE(music_name, 'Bard', 'QA')
+        WHEN
+            music_name LIKE '%Amar%'
+        THEN
+            REPLACE(music_name,
+                'Amar',
+                'Code Review')
+        WHEN
+            music_name LIKE '%Pais%'
+        THEN
+            REPLACE(music_name,
+                'Pais',
+                'Pull Requests')
+        WHEN music_name LIKE '%SOUL%' THEN REPLACE(music_name, 'SOUL', 'CODE')
+        WHEN
+            music_name LIKE '%SUPERSTAR%'
+        THEN
+            REPLACE(music_name,
+                'SUPERSTAR',
+                'SUPERDEV')
+        ELSE NULL
+    END AS novo_nome
+FROM
+    SpotifyClone.music
+WHERE
+    music_name LIKE '%Bard%'
+        OR  music_name LIKE '%Amar%'
+        OR  music_name LIKE '%Pais%'
+        OR  music_name LIKE '%SOUL%'
+        OR  music_name LIKE '%SUPERSTAR%'
+ORDER BY  music_name DESC;
