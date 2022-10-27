a842cc1e41a9e43b4dfcdb8edf1b51763af7ef99
Matheus <matheuseduardo.jp@gmail.com>
2022-10-27 01:09:45 -0300

    FEAT: Requisito 04

--- desafio4.sql
+++ desafio4.sql
@@ -1,26 +1,11 @@
-DROP DATABASE IF EXISTS SpotifyClone;
-
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
-
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
-
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+SELECT 
+    user_name AS usuario,
+	IF(YEAR(MAX(H.historic_date_reproduction)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo') AS status_usuario
+FROM
+    SpotifyClone.user AS U
+        INNER JOIN
+    SpotifyClone.historic AS H ON U.user_id = H.user_id
+GROUP BY user_name
+ORDER BY user_name;
